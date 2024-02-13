72557b462aae53c9491d8f514488b3f6281f9c29
amanst1 <seifeamanuel@gmail.com>
2024-02-13 17:59:55 +0300

    add form to general info jsx, remove unused imports from app jsx

---
+++ src/components/GeneralInfo.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react"
+
+export default function GeneralInfo() {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [phone, setPhone] = useState('')
+
+    return(
+        <div>
+            <form action="">
+               <label htmlFor="name">Name: </label> 
+               <input id="name" name="name" type="text" value={name} onChange={(e) => setName(e.target.value)}/> <br/>
+               <label htmlFor="email">Email: </label> 
+               <input id="email" name="email" type="email" value={email} onChange={(e) => setEmail(e.target.value)} /> <br/>
+               <label htmlFor="phone">Phone: </label> 
+               <input id="phone" name="phone" type="tel" value={phone} onChange={(e) => setPhone(e.target.value)}/> <br/>
+            </form>
+        </div>
+    )
+}
